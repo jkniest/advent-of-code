8749f0913d94af0d32e284cfbf5630df5577183b
Jordan Kniest <j.kniest@basecom.de>
2022-12-06 21:34:44 +0100

    2022, day 6

---
+++ 2022/06/task2.php
@@ -0,0 +1,15 @@
+<?php
+
+$input = file_get_contents(__DIR__.'/input.txt');
+$count = strlen($input);
+
+for($i = 3; $i < $count; $i++) {
+    $buffer = [];
+    for ($n = $i; $n >= $i - 13; $n--) {
+        if (in_array($input[$n], $buffer)) continue 2;
+        $buffer[] = $input[$n];
+    }
+    break;
+}
+
+print('First marker at character: ' . ($i + 1) . "\n");
