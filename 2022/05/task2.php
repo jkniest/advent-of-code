033aaaf7d5b24784171ff98b0ab39f98c591579c
Jordan Kniest <j.kniest@basecom.de>
2022-12-06 21:27:31 +0100

    2022, day 5

---
+++ 2022/05/task2.php
@@ -0,0 +1,45 @@
+<?php
+
+$input = explode("\n", file_get_contents(__DIR__.'/input.txt'));
+
+$stacks = [];
+
+for ($i = 0, $count = count($input); $i < $count; $i++) {
+    if (empty($input[$i])) break;
+
+    // Check for each three characters, ignorning the empty space between
+    $stack = 1;
+    for ($lI = 0, $lIMax = strlen($input[$i]); $lI < $lIMax; $lI += 4, $stack++) {
+        $box = substr($input[$i], $lI, 3);
+        if (empty(trim($box)) || !str_starts_with($box, '[')) {
+            continue;
+        }
+
+        $stacks[$stack] ??= [];
+        array_unshift($stacks[$stack], str_replace(['[', ']'], '', $box));
+    }
+}
+
+for ($i = $i + 1, $iMax = count($input); $i < $iMax; $i++) {
+    preg_match_all('/\d+/', $input[$i], $matches);
+    [$amount, $from, $to] = $matches[0];
+
+    $localStack = [];
+    for ($aI = 0; $aI < (int)$amount; $aI++) {
+        $localStack[] = array_pop($stacks[(int)$from]);
+    }
+
+    $stacks[(int)$to] = array_merge(
+        $stacks[(int)$to],
+        array_reverse($localStack)
+    );
+}
+
+ksort($stacks);
+
+$tops = array_map(
+    fn(array $items) => $items[count($items) - 1],
+    $stacks
+);
+
+print('Top stacks: ' . implode('', $tops) . "\n");
