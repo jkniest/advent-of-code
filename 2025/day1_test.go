5173e9d556ec843b689c0599bedf324eb85b1bd6
Jordan Kniest <mail@jkniest.de>
2025-12-04 12:44:59 +0100

    2025, day 1.2

--- 2025/day1_test.go
+++ 2025/day1_test.go
@@ -13,7 +13,7 @@ import (
 func TestDay1(t *testing.T) {
 	t.Run("part 1", func(t *testing.T) {
 		// Read input
-		file, err := os.Open("inputs/day1_1.txt")
+		file, err := os.Open("inputs/day1.txt")
 		require.NoError(t, err)
 		t.Cleanup(func() {
 			err := file.Close()
@@ -43,6 +43,50 @@ func TestDay1(t *testing.T) {
 			if value == 0 {
 				wasAtZero++
 			}
+		}
+
+		t.Logf("Result = %d", wasAtZero)
+	})
+
+	t.Run("part 2", func(t *testing.T) {
+		// Read input
+		file, err := os.Open("inputs/day1.txt")
+		require.NoError(t, err)
+		t.Cleanup(func() {
+			err := file.Close()
+			require.NoError(t, err)
+		})
+
+		scanner := bufio.NewScanner(file)
+
+		value := 50
+		wasAtZero := 0
+
+		for scanner.Scan() {
+			// Split text into direction and amount
+			text := scanner.Text()
+			runeString := utf8string.NewString(text)
+			direction := runeString.At(0)
+
+			amount, err := strconv.Atoi(text[1:])
+			require.NoError(t, err)
+
+			if direction == 'L' {
+				for range amount {
+					value = (value - 1 + 100) % 100
+					if value == 0 {
+						wasAtZero++
+					}
+				}
+			} else {
+				for range amount {
+					value = (value + 1) % 100
+					if value == 0 {
+						wasAtZero++
+					}
+				}
+			}
+
 		}
 
 		t.Logf("Result = %d", wasAtZero)
